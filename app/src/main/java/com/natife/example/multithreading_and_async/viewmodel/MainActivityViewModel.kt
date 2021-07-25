83d3b0d7670e03c2af289bd4500c6aa532ed8cb9
lexusshatov <lexusshatov2@gmail.com>
2021-07-25 20:43:19 +0300

    coroutines

--- app/src/main/java/com/natife/example/multithreading_and_async/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/natife/example/multithreading_and_async/viewmodel/MainActivityViewModel.kt
@@ -1,24 +1,16 @@
 package com.natife.example.multithreading_and_async.viewmodel
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.flow
 import kotlin.random.Random
 
-class MainActivityViewModel: ViewModel() {
+class MainActivityViewModel : ViewModel() {
     private val random = Random(System.nanoTime())
-    val data: StateFlow<Int> by lazy {
-        val dataFlow = MutableStateFlow(0)
-        viewModelScope.launch(Dispatchers.IO) {
-            while (true) {
-                dataFlow.emit(random.nextInt(0, 1000))
-                delay(random.nextInt(0, 2000).toLong())
-            }
+    val data = flow {
+        while (true) {
+            emit(random.nextInt(0, 1000))
+            delay(random.nextInt(0, 2000).toLong())
         }
-        dataFlow
     }
 }
