c463796c41861eb3ff6cd231573e3a98149b22e7
lexusshatov <lexusshatov2@gmail.com>
2021-07-25 14:06:26 +0300

    rxjava

--- app/src/main/java/com/natife/example/multithreading_and_async/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/natife/example/multithreading_and_async/viewmodel/MainActivityViewModel.kt
@@ -3,10 +3,6 @@ package com.natife.example.multithreading_and_async.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import kotlin.concurrent.thread
 import kotlin.random.Random
 
