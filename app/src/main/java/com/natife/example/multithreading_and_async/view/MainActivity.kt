63fd5993b840e385f335d9aa1a5afd8e83c66dc1
Native Developer <eugene.bychkailo.natife@gmail.com>
2021-07-26 15:18:48 +0300

    coroutines

--- app/src/main/java/com/natife/example/multithreading_and_async/view/MainActivity.kt
+++ app/src/main/java/com/natife/example/multithreading_and_async/view/MainActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.natife.example.multithreading_and_async.databinding.ActivityMainBinding
 import com.natife.example.multithreading_and_async.viewmodel.MainActivityViewModel
@@ -18,7 +16,7 @@ import kotlinx.coroutines.withContext
 class MainActivity : AppCompatActivity() {
     private val viewModel by viewModels<MainActivityViewModel>()
     private lateinit var binding: ActivityMainBinding
-    private val adapter = ItemAdapter<String>()
+    private val adapter = ItemAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +28,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         lifecycleScope.launch(Dispatchers.IO) {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
+            lifecycleScope.launchWhenStarted {
                 viewModel.data.collect {
                     withContext(Dispatchers.Main) {
                         Log.d("TEST", "Item: $it")
